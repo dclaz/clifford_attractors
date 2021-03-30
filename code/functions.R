00d23e07e6e0620d7e9d8655c2228d2c058a7bfb
David Lazaridis <40458315+dclaz@users.noreply.github.com>
2021-03-30 16:26:21 +1100

    added more colour schemes

--- code/functions.R
+++ code/functions.R
@@ -11,78 +11,91 @@ cliff_rcpp <- cppFunction("
 
 
 # Function for mapping a point to a colour
-map2color <- function(x,pal,limits=NULL){
+map2color <- function(x, pal, limits=NULL){
     if(is.null(limits)) limits=range(x)
     pal[findInterval(x,seq(limits[1],limits[2],length.out=length(pal)+1), all.inside=TRUE)]
 }
 
 
-# Plot Clifford Attractor
-plot_cliff <- function(
-    n_iter, params, alpha, res=c(2560, 1440), colours="plasma", output_loc=""
-){
-    
-    # Obtain matrix of points
-    cliff_points <- cliff_rcpp(
-        n_iter, params[1], params[2], params[3], params[4]
-    )
-    
-    # Calculate angle between successive points
-    cliff_angle <- atan2(
-        (cliff_points[,1] - c(cliff_points[-1,1],0)), 
-        (cliff_points[,2] - c(cliff_points[-1,2],0))
-    )
-    
-    # Obtain colours for points
-    if (colours == "plasma"){
-        available_cols <- plasma(1024, alpha = alpha, begin = 0, end = 1, direction = 1)
-        cliff_cols <- map2color(
-            cliff_angle, 
-            c(available_cols, rev(available_cols))
-        )
-    } else if (colours == "viridis"){
-        available_cols <- viridis(1024, alpha = alpha, begin = 0, end = 1, direction = 1)
-        cliff_cols <- map2color(
-            cliff_angle, 
-            c(available_cols, rev(available_cols))
-        )
-    } else if (colours == "magma"){
-        available_cols <- magma(1024, alpha = alpha, begin = 0, end = 1, direction = 1)
-        cliff_cols <- map2color(
-            cliff_angle, 
-            c(available_cols, rev(available_cols))
-        )
-    } else {
-        cliff_cols = rgb(0, 0, 0, alpha = alpha)
-    }
-    
+# # Plot Clifford Attractor #OLD FUNCTION
+# plot_cliff <- function(
+#     n_iter, params, alpha, res=c(2560, 1440), colours="plasma", output_loc=""
+# ){
+#     
+#     # Obtain matrix of points
+#     cliff_points <- cliff_rcpp(
+#         n_iter, params[1], params[2], params[3], params[4]
+#     )
+#     
+#     # Calculate angle between successive points
+#     cliff_angle <- atan2(
+#         (cliff_points[,1] - c(cliff_points[-1,1],0)), 
+#         (cliff_points[,2] - c(cliff_points[-1,2],0))
+#     )
+#     
+#     # Obtain colours for points
+#     if (colours == "plasma"){
+#         available_cols <- plasma(1024, alpha = alpha, begin = 0, end = 1, direction = 1)
+#         cliff_cols <- map2color(
+#             cliff_angle, 
+#             c(available_cols, rev(available_cols))
+#         )
+#     } else if (colours == "viridis"){
+#         available_cols <- viridis(1024, alpha = alpha, begin = 0, end = 1, direction = 1)
+#         cliff_cols <- map2color(
+#             cliff_angle, 
+#             c(available_cols, rev(available_cols))
+#         )
+#     } else if (colours == "magma"){
+#         available_cols <- magma(1024, alpha = alpha, begin = 0, end = 1, direction = 1)
+#         cliff_cols <- map2color(
+#             cliff_angle, 
+#             c(available_cols, rev(available_cols))
+#         )
+#     } else {
+#         cliff_cols = rgb(0, 0, 0, alpha = alpha)
+#     }
+#     
+# 
+#     
+#     # Name of output file
+#     output_file <- paste0(
+#         output_loc,
+#         format(Sys.time(), "%Y%m%d_%H%M%S"),
+#         "_clifford_",
+#         params[1],"_",params[2],"_",params[3],"_",params[4],"_",n_iter/10^6,
+#         ".png"
+#     )
+#     
+#     # Output image directly to disk
+#     png(
+#         output_file,
+#         width = res[1],
+#         height = res[2],
+#         pointsize = 1,
+#         bg = "white",
+#         antialias = "cleartype"
+#         
+#     )
+#         plot(
+#             cliff_points[-1,],
+#             bg = "white",
+#             pch = ".",
+#             col = cliff_cols
+#         )
+#     dev.off()
+# }
 
-    
-    # Name of output file
-    output_file <- paste0(
-        output_loc,
-        format(Sys.time(), "%Y%m%d_%H%M%S"),
-        "_clifford_",
-        params[1],"_",params[2],"_",params[3],"_",params[4],"_",n_iter/10^6,
-        ".png"
-    )
-    
-    # Output image directly to disk
-    png(
-        output_file,
-        width = res[1],
-        height = res[2],
-        pointsize = 1,
-        bg = "white",
-        antialias = "cleartype"
-        
-    )
-        plot(
-            cliff_points[-1,],
-            bg = "white",
-            pch = ".",
-            col = cliff_cols
-        )
-    dev.off()
-}
 
+
+# Special vaporwave colour palette
+vapor_pal_1 <- colorRampPalette(
+    c(
+        "#ff00c1", #	(255,0,193)
+        "#9600ff", #	(150,0,255)
+        "#4900ff", #	(73,0,255)
+        "#00b8ff", #	(0,184,255)
+        "#00fff9" #	(0,255,249)
+    ),
+    interpolate = c("linear")
+)
--- faster_clifford_attractor.R
+++ faster_clifford_attractor.R
@@ -1,35 +1,32 @@
-##########################################################################################
+################################################################################
 # Load packages
 library(Rcpp)
 library(viridis)
 library(scattermore)
-library(png)
+library(stringr)
 
+################################################################################
 # Clean workspace
 rm(list=ls())
 gc()
 
 # Load in awesome functions 
-setwd("D:\\Dropbox\\Experiments\\Clifford_Attractors\\")
 source("code/functions.R")
 
+################################################################################
+# Settings
+n_plots <- 1000
 
-
-n_plots <- 100
+output_loc <- "D:/dump/clifford/"
+n_iter <- 100*10^6
+alpha <- 0.025
+res <- 2*c(2560, 1440)
+pointsize <- 1
 
 # Set Seed
 #set.seed(1234561)
 
-output_loc <- "D:/dump/clifford/"
-n_iter = 100*10^6
-alpha = 0.025
-res = 2*c(2560, 1440)
-
-
 ##########################################################################################
-
-
-
 # Generate random set of params
 params <- matrix(
     round(runif(4*n_plots, -3, 3), 4),
@@ -38,18 +35,23 @@ params <- matrix(
 
 # TEST SET
 #params = matrix(c(1.359, 1.126, -1.793, -1.336), nrow=1)
-#colour_option = "C"
+#colour_option = "V"
 
+
+
+##########################################################################################
 # Loop that saves down png's of the clifford attractos
 for (i in 1:n_plots){
+    
+    current_params = params[i,]
     # Print status
     cat(
         as.character(Sys.time()),
-        paste0("plot: ", i,"\tparameters: ", paste0(params[i,], collapse=", ")),
+        paste0("plot: ", i,"\tparameters: ", paste0(current_params, collapse=", ")),
         "\n"
     )
     
-    #colour_option = sample(c("A", "B", "C", "D", "E", "BW"), 1) # cividis is meh
+    colour_option = sample(c("A", "B", "C", "D", "E", "BW", "V"), 1) # cividis is meh
     
     # Obtain matrix of points
     # Print status
@@ -59,7 +61,7 @@ for (i in 1:n_plots){
         "\n"
     )
     cliff_points <- cliff_rcpp(
-        n_iter, params[1], params[2], params[3], params[4]
+        n_iter, current_params[1], current_params[2], current_params[3], current_params[4]
     )
     
     # Calculate angle between successive points
@@ -71,12 +73,23 @@ for (i in 1:n_plots){
     # Obtain colours for points
     cat(
         as.character(Sys.time()),
-        paste0("\tObtaining colours\t", colour_option),
+        paste0("\tObtaining colours:\t", colour_option),
         "\n"
     )
     if (colour_option == "BW"){
         cliff_cols = rgb(0, 0, 0, alpha = alpha)
-        cliff_cols_mat = drop(cliff_cols_mat)
+        #cliff_cols_mat = drop(cliff_cols)
+    } else if (colour_option == "V"){
+        available_cols = vapor_pal_1(1024*16)
+        available_cols = paste0(
+            available_cols,
+            str_pad(as.hexmode(round(alpha*255)), width=2, pad="0", side="left")
+        )
+        cliff_cols <- map2color(
+            cliff_angle, 
+            c(available_cols, rev(available_cols))
+        )
+        cliff_cols <- cliff_cols[-length(cliff_cols)]
     } else {
         available_cols <- viridis(
             n = 1024*16, 
@@ -90,50 +103,75 @@ for (i in 1:n_plots){
             cliff_angle, 
             c(available_cols, rev(available_cols))
         )
+        cliff_cols <- cliff_cols[-length(cliff_cols)]
         # convert to matrix
-        cliff_cols_mat = col2rgb(cliff_cols, alpha = TRUE)[,-1]
+        #cliff_cols_mat = col2rgb(cliff_cols, alpha = TRUE)[,-1]
     }
     
 
-    
-    # Name of output file
-    output_file <- paste0(
-        output_loc,
-        format(Sys.time(), "%Y%m%d_%H%M%S"),
-        "_clifford_", colour_option, "_",
-        params[1],"_",params[2],"_",params[3],"_",params[4],"_",n_iter/10^6,
-        ".png"
-    )
-    
-    # Output image directly to disk
+    # Only bother plotting if there is a good number of points
+    n_unique_points_in_sample = nrow(unique(cliff_points[1:1000000,]))
     cat(
         as.character(Sys.time()),
-        paste0("\tPlotting.")
+        paste0("\tUnique points in sample:\t", n_unique_points_in_sample),
+        "\n"
     )
-    png(
-        output_file,
-        width = res[1],
-        height = res[2],
-        pointsize = 1,
-        bg = "white",
-        antialias = "cleartype"
+    if (n_unique_points_in_sample > 10000){
+    
+        # Name of output file
+        output_file <- paste0(
+            output_loc,
+            format(Sys.time(), "%Y%m%d_%H%M%S"),
+            "_clifford_", colour_option, "_", pointsize, "_",
+            current_params[1],"_",current_params[2],"_",current_params[3],"_",current_params[4],"_",n_iter/10^6,
+            ".jpg"
+        )
         
-    )
-    cat(".")
-    # NEW method
-    plot(scattermore(
-        xy = cliff_points[-1,],
-        size = res,
-        rgba = cliff_cols_mat,
-        cex = 1,
-        output.raster = TRUE
-    ))
-    cat(".")
-    dev.off()
-    cat(". Done")
+        # Output image directly to disk
+        cat(
+            as.character(Sys.time()),
+            paste0("\tPlotting.")
+        )
+        
+        jpeg(
+            output_file,
+            width = res[1],
+            height = res[2],
+            pointsize = 1,
+            bg = "white",
+            antialias = "cleartype",
+            quality = 100
     
+        )
+        cat(".")
+
+        cliff_points_1 <- cliff_points[-1,1]
+        cliff_points_2 <- cliff_points[-1,2]
+        rm(cliff_points)
+        gc()
     
+        scattermoreplot(
+            cliff_points_1,
+            cliff_points_2,
+            bg = "white",
+            cex = pointsize,
+            col = cliff_cols
+        )
+    
+        cat(".")
+        dev.off()
+        cat(". Done\n")
+        
+    } else {
+        cat(
+            as.character(Sys.time()),
+            paste0("\tSkipping plot.\n")
+        )
+    }
+
+
 }
 
+# BW colour issue remains
 
 
